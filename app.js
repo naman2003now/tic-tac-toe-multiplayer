0fa6deb0add1a8ad5b112d43e409a53f33a9ef8a
Naman Agrawal <a6.namanagrawal@gmail.com>
2022-01-25 19:33:34 +0530

    half way done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const http = require('http')
 const path = require('path')
+const { debugPort } = require('process')
 const socketio = require('socket.io')
 
 const app = express()
@@ -9,11 +10,38 @@ const io = socketio(server)
 
 const port = process.env.PORT || 6969
 
+var db = []
+
 io.on("connection", (socket) => {
     console.log("Connection Reached")
     socket.on('join', ({name, roomName}) => {
-        socket.join(roomName)
-        
+        players_in_room = db.filter((user) => user.roomName == roomName)
+        if(players_in_room.length > 1){
+            socket.emit('full')
+        }
+        else{
+            if(players_in_room.length == 1){
+                if(players_in_room[0].name == name){
+                    socket.emit('name')
+                    socket.to(roomName).emit('name')
+                    db = db.filter((player) => player.roomName != roomName)
+                }else
+                {
+                    socket.join(roomName)
+                    socket.emit('connected', {player1: name, player2: players_in_room[0].name, playas:"X"})
+                    socket.to(roomName).emit('connected', {player1: players_in_room[0].name, player2: name, playas:"O"})
+                    db.push({id: socket.id, name, roomName, score: 0})
+                }
+            }
+            else{
+                socket.join(roomName)
+                db.push({id: socket.id, name, roomName, score: 0})
+            }
+        }
+    })
+
+    socket.on('play', (position) => {
+        socket.to(db.filter((user) => user.id == socket.id)[0].roomName).emit('update', position)
     })
 })
 
--- public/js/game.js
+++ public/js/game.js
@@ -1,36 +1,89 @@
-io()
+let socket = io()
 
-function mark(currentMark){
-    if (currentMark == "X"){
-        return "O"
-    }
-    else{
-        return "X"
+function roomInfo(){
+    rawData = location.search
+    return {
+        name: rawData.substring(10, rawData.search("&")).replace("+", " "),
+        roomName: rawData.substring(rawData.search("&")+10, rawData.length)
     }
 }
 
-cells = {
-    topLeft: document.getElementById("TL"),
-    topMiddle: document.getElementById("TM"),
-    topRight: document.getElementById("TR"),
+//Connecting to the room
+socket.emit('join', roomInfo())
+socket.on('full', () => {
+    alert("The room is full")
+    location.replace("/")
+})
+socket.on('name', () => {
+    location.replace("/")
+    alert("An error occured caused the game to crash \n(Might be an identity theft, or you refreshed the page)")
+})
 
-    Left: document.getElementById("L"),
-    Middle: document.getElementById("M"),
-    Right: document.getElementById("R"),
+socket.on('connected', ({player1, player2, playas}) => {
+    const playingas = playas
+    var turn = playas == "X"
+    function play(position, mark){
+        turn = !turn
+        document.getElementById(position).innerHTML = mark
+    }
 
-    bottomLeft: document.getElementById("BL"),
-    bottomMiddle: document.getElementById("BM"),
-    bottomRight: document.getElementById("BR")
-}
+    function mark(position){
+        if(turn === true){
+            if(document.getElementById(position).innerHTML == " "){
+                socket.emit('play', position)
+                play(position, playingas)
+            }
+        }
+    }
 
-cells.topLeft.onclick = () => {cells.topLeft.innerHTML = mark(cells.topLeft.innerHTML)}
-cells.topMiddle.onclick = () => {cells.topMiddle.innerHTML = mark(cells.topMiddle.innerHTML)}
-cells.topRight.onclick = () => {cells.topRight.innerHTML = mark(cells.topRight.innerHTML)}
-cells.Left.onclick = () => {cells.Left.innerHTML = mark(cells.Left.innerHTML)}
-cells.Middle.onclick = () => {cells.Middle.innerHTML = mark(cells.Middle.innerHTML)}
-cells.Right.onclick = () => {cells.Right.innerHTML = mark(cells.Right.innerHTML)}
-cells.bottomLeft.onclick = () => {cells.bottomLeft.innerHTML = mark(cells.bottomLeft.innerHTML)}
-cells.bottomMiddle.onclick = () => {cells.bottomMiddle.innerHTML = mark(cells.bottomMiddle.innerHTML)}
-cells.bottomRight.onclick = () => {cells.bottomRight.innerHTML = mark(cells.bottomRight.innerHTML)}
+    socket.on('update', (position) => {
+        if(playingas == "X"){
+            play(position, "O") 
+        }
+        else{
+            play(position, "X")
+        }
+    })
+
+    document.getElementById("Title").innerHTML = player1 + " VS " + player2
+    document.getElementsByClassName("opponent")[0].innerHTML = player2 + ": 0"
+
+    cells = {
+        topLeft: document.getElementById("TL"),
+        topMiddle: document.getElementById("TM"),
+        topRight: document.getElementById("TR"),
+    
+        Left: document.getElementById("L"),
+        Middle: document.getElementById("M"),
+        Right: document.getElementById("R"),
+    
+        bottomLeft: document.getElementById("BL"),
+        bottomMiddle: document.getElementById("BM"),
+        bottomRight: document.getElementById("BR")
+    }
+    
+    cells.topLeft.onclick = () => {mark(cells.topLeft.id)}
+    cells.topMiddle.onclick = () => {mark(cells.topMiddle.id)}
+    cells.topRight.onclick = () => {mark(cells.topRight.id)}
+    cells.Left.onclick = () => {mark(cells.Left.id)}
+    cells.Middle.onclick = () => {mark(cells.Middle.id)}
+    cells.Right.onclick = () => {mark(cells.Right.id)}
+    cells.bottomLeft.onclick = () => {mark(cells.bottomLeft.id)}
+    cells.bottomMiddle.onclick = () => {mark(cells.bottomMiddle.id)}
+    cells.bottomRight.onclick = () => {mark(cells.bottomRight.id)}
+
+    cells.topLeft.innerHTML = " "
+    cells.topMiddle.innerHTML = " "
+    cells.topRight.innerHTML = " "
+    cells.Left.innerHTML = " "
+    cells.Middle.innerHTML = " "
+    cells.Right.innerHTML = " "
+    cells.bottomLeft.innerHTML = " "
+    cells.bottomMiddle.innerHTML = " "
+    cells.bottomRight.innerHTML = " "
+
+})
+
+
 
 
